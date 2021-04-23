2ffb3251baab9153e0f41beefe872fe10f09e2de
thor-ium <thbr2284@colorado.edu>
2021-04-22 21:10:09 -0600

    Header first commit

---
+++ linkedGit.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#ifndef LINKEDGIT_H
+#define LINKEDGIT_H
+
+#include <string>
+
+using namespace std;
+
+struct singlyNode {
+    string fileName;
+    string fileVersion;
+    singlyNode* next;
+};
+
+struct doublyNode {
+    int commitNumber;
+    singlyNode* head;
+    doublyNode* previous;
+    doublyNode* next;
+};
+
+class Git {
+    private:
+        singlyNode* head;
+        doublyNode* comHead;
+    public:
+        Git();
+        ~Git();
+        void init();
+        void add();
+        void remove();
+        int commit();
+        void checkout();
+};
+
+#endif
